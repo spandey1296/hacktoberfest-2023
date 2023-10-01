e7546a6bed9a989d375e6514503ba038184d03d8
Dhruvil Soni <dhruvilsoni1703@gmail.com>
2023-10-01 18:00:44 +0530

    Add files via upload

---
+++ React-03/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement(
+    "h1",
+    {
+        style: {
+            color: "orange",
+        },
+        key: "1",
+    },
+    "Namaste React"
+);
+const heading2 = React.createElement(
+    "h2",
+    {
+        key: "2",
+    },
+    "Namaste React"
+);
+const heading3 = React.createElement(
+    "h3",
+    {
+        key: "3",
+    },
+    "Namaste React"
+);
+const container = React.createElement(
+    "div",
+    {
+        id: "container",
+    },
+    [heading1, heading2, heading3]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
